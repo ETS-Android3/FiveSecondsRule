df99d3024aa3d641bafebe9548c905a0d5478023
Arthur <arthur.gabzalilov@gmail.com>
2021-07-19 21:46:17 +0300

    Add some filters

--- app/src/main/java/com/example/a5sec/FiltersActivity.java
+++ app/src/main/java/com/example/a5sec/FiltersActivity.java
@@ -2,7 +2,10 @@ package com.example.a5sec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FiltersActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class FiltersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void Less_Button(View view){
+        EditText ET = findViewById(R.id.editTextTextPersonName);
+        int number = Integer.parseInt(ET.getText().toString());
+        if (number > 1)
+        {
+            number -= 1;
+            ET.setText(number+ "");
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void More_Button(View view){
+        EditText ET = findViewById(R.id.editTextTextPersonName);
+        int number = Integer.parseInt(ET.getText().toString());
+        if (number < 30)
+        {
+            number += 1;
+            ET.setText(number+ "");
+        }
     }
 }
