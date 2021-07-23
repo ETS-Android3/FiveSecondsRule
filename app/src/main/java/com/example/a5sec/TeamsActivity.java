8b4a470f641977b55817b27773c590b766def10c
Arthur <arthur.gabzalilov@gmail.com>
2021-07-23 23:04:36 +0500

    Add teams filter

--- app/src/main/java/com/example/a5sec/TeamsActivity.java
+++ app/src/main/java/com/example/a5sec/TeamsActivity.java
@@ -3,12 +3,45 @@ package com.example.a5sec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TeamsActivity extends AppCompatActivity {
 
+    ListView listview;
+    Button addButton;
+    EditText GetValue;
+    String[] ListElements = new String[] {
+            "Cats",
+            "Dogs",
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
+
+        listview = findViewById(R.id.listView1);
+        addButton = findViewById(R.id.button7);
+        GetValue = findViewById(R.id.editText1);
+
+        final List<String> ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (TeamsActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+        listview.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListElementsArrayList.add(GetValue.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
