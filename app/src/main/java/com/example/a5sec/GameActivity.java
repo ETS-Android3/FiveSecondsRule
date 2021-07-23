28c5416b471171a63e17b96e30419b584a150bf6
Arthur <arthur.gabzalilov@gmail.com>
2021-07-23 16:55:27 +0500

    Button changes name
    Button changes name from "Go!" to "Answer" during game

--- app/src/main/java/com/example/a5sec/GameActivity.java
+++ app/src/main/java/com/example/a5sec/GameActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -26,11 +27,14 @@ public class GameActivity extends AppCompatActivity {
             time_bar.setText((int)l/1000+" : "+(l/10)%100);
         }
 
+        @SuppressLint("SetTextI18n")
         @Override
         public void onFinish() {
             TextView time_bar = findViewById(R.id.textView3);
             time_bar.setText("Time's up!");
             is_timer_on = false;
+            Button button = findViewById(R.id.button3);
+            button.setText(getResources().getString(R.string.stop_botton));
         }
     };
 
@@ -39,11 +43,15 @@ public class GameActivity extends AppCompatActivity {
         {
             timer.start();
             is_timer_on = true;
+            Button button = findViewById(R.id.button3);
+            button.setText(getResources().getString(R.string.answer_button));
         }
         else
         {
             timer.cancel();
             is_timer_on = false;
+            Button button = findViewById(R.id.button3);
+            button.setText(getResources().getString(R.string.stop_botton));
         }
         String a = "lolik";
         final TextView tw = findViewById(R.id.textView2);
