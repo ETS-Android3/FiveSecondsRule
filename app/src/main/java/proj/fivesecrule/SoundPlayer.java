3c8e487c42857c02e2ee7963321355881977cab9
Arthur <arthur.gabzalilov@gmail.com>
2022-02-22 14:39:35 +0500

    add small changes

--- app/src/main/java/proj/fivesecrule/SoundPlayer.java
+++ app/src/main/java/proj/fivesecrule/SoundPlayer.java
@@ -1,32 +1,21 @@
 package proj.fivesecrule;
 
 import android.content.Context;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 
 public class SoundPlayer {
-    private SoundPool soundPool;
-    private int mStreamId;
-    private Context context;
+    private MediaPlayer mediaPlayer;
+
     public SoundPlayer(Context cnt){
-        context = cnt;
-        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 100);
-        soundPool.load(context, R.raw.timer, 1);
+        mediaPlayer = MediaPlayer.create(cnt, R.raw.timer);
     }
 
     public void play_sound(){
-        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        float curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        float maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        int priority = 1;
-        int no_loop = 0;
-        float normal_playback_rate = 1f;
-        int mSoundId = 1;
-        mStreamId = soundPool.play(mSoundId, curVolume, curVolume, priority, no_loop,
-                normal_playback_rate);
+        mediaPlayer.start();
     }
 
     public void stop_sound(){
-        soundPool.stop(mStreamId);
+        mediaPlayer.stop();
+        mediaPlayer.reset();
     }
 }
